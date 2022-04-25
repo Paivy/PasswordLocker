d7d9608cb96c616769aad4ada76898d5ffbbc001
Paivy <paivyeshirera@gmail.com>
2022-04-25 10:10:01 +0300

    add the TearDown function

--- user-test.py
+++ user-test.py
@@ -4,3 +4,5 @@ from user import User
 class TestUser(unittest.TestCase):
     def setUp(self):
         self.new_user = User("Paivy","paivy123")
+    def tearDown(self):
+        User.userslist = []
