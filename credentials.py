9c480126581a34619aa0c2b200f9ff1ea1113226
Paivy <paivyeshirera@gmail.com>
2022-04-25 10:19:31 +0300

    create the init method to define properties for our object self

--- credentials.py
+++ credentials.py
@@ -4,3 +4,11 @@ class Credentials:
     """
     This is a class that generates new instances of Credentials
     """ 
+    accounts = []
+    def __init__(self,accountusername,accountname,accountpassword):
+        """
+        __init__ method helps us define properties for our objectself.
+        """
+        self.accountusername   =accountusername
+        self.accountname =accountname
+        self.accountpassword = accountpassword
