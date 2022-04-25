f49ffcb67c6ba8bf26ec10df9bea0fa9c886d1e2
Paivy <paivyeshirera@gmail.com>
2022-04-25 10:31:46 +0300

    add the display_user function and the unitest function to test the objects in self

--- credentials-test.py
+++ credentials-test.py
@@ -20,3 +20,18 @@ class TestCredentials(unittest.TestCase):
         """
         test_delete_contact to test if we can remove an account from our accounts list
         """
+        self.new_account.save_account()
+        test_account = Credentials("Paivy","paivy123","paivy123")
+        test_account.save_account()
+        self.new_account.delete_account()
+        self.assertEqual(len (Credentials.accounts),1)
+    
+
+  
+        
+    def test_display_user(self):
+        self.assertEqual(Credentials.display_account(), Credentials.accounts)
+        
+
+if __name__ == '__main__':
+    unittest.main()
