dbef895b6622225656892e2d00cac30f8fe4734a
Paivy <paivyeshirera@gmail.com>
2022-04-25 10:01:54 +0300

    used the init method to save user informmation

--- user.py
+++ user.py
@@ -1,8 +1,17 @@
 import random
 import string
+
 class User:
     """
-    This is a class that generates instances of the user
+    This is a class that generates instances of the class
     """
+    userslist=[]
 
-   
+    def __init__(self,username,password):
+        """
+        __init__ method helps us define properties for our objects which is self
+        """
+        
+        self.username=username
+        self.password=password
+       
