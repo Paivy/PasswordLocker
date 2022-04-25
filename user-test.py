48e3e74417ab348e22073ec13b9ec8af272bea7d
Paivy <paivyeshirera@gmail.com>
2022-04-25 10:15:56 +0300

    add the unitest function to make the user-test run

--- user-test.py
+++ user-test.py
@@ -9,3 +9,20 @@ class TestUser(unittest.TestCase):
     def test_init(self):
         self.assertEqual(self.new_user.username,"Paivy")
         self.assertEqual(self.new_user.password,"paivy123")
+    def test_delete_user(self):
+        self.new_user.save_user()
+        test_user = User("Paivy","paivy123")
+        test_user.save_user()
+        self.new_user.delete_user()
+        self.assertEqual(len(User.userslist),1)
+
+
+
+
+    """
+    Test class that defines testcases for the user class behaviours
+    Args:
+    unittest.Testcase: TestCase class that helps in creating testcases
+    """
+if __name__ == '__main__':
+    unittest.main()
