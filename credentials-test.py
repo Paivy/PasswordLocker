c42cf4f3d2c1b94a87931146f254a496156bc640
Paivy <paivyeshirera@gmail.com>
2022-04-25 10:28:37 +0300

    defined test_init to test the self objects

--- credentials-test.py
+++ credentials-test.py
@@ -8,3 +8,8 @@ class TestCredentials(unittest.TestCase):
     def tearDown(self):
         Credentials.accounts = []
 
+    def test_init(self):
+        self.assertEqual(self.new_account.accountusername,"Paivy")
+        self.assertEqual(self.new_account.accountname,"paivy123")
+        self.assertEqual(self.new_account.accountpassword,"paivy123")
+        
