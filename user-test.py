59159631e2b42b6b8405952c2c674f7545466670
Paivy <paivyeshirera@gmail.com>
2022-04-25 10:13:47 +0300

    add test_init to test the objects in self

--- user-test.py
+++ user-test.py
@@ -6,3 +6,6 @@ class TestUser(unittest.TestCase):
         self.new_user = User("Paivy","paivy123")
     def tearDown(self):
         User.userslist = []
+    def test_init(self):
+        self.assertEqual(self.new_user.username,"Paivy")
+        self.assertEqual(self.new_user.password,"paivy123")
